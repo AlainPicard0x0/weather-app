6c97756fe42971ce30899770b311a17ff336ee5d
Alain Picard <46497761+DevCoder0x0@users.noreply.github.com>
2020-05-05 20:58:13 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,55 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    let tempSummary = document.querySelector(".temp-summary");
+    let temp = document.querySelector(".temp");
+    let location = document.querySelector(".location");
+    const tempSection = document.querySelector(".temp-section");
+    const tempSpan = document.querySelector(".temp-section span");
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log(position);
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+            const api = `${ proxy }https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${ lat },${ long }`;
+            fetch(api)
+            .then(data => {
+                return data.json()
+            })
+            .then(data => {
+                console.log(data);
+                const { temperature, summary, icon } = data.currently;
+                temp.textContent = temperature;
+                tempSummary.textContent = summary;
+                location.textContent = data.timezone;
+                let celcius = (temperature - 32) * (5 / 9);  
+                celcius = Math.round(celcius * 100) / 100;              
+                setIcons(icon, document.querySelector(".icon"));
+                tempSection.addEventListener("click", () => {
+                    if(tempSpan.textContent === "F") {
+                        tempSpan.textContent = "C";
+                        temp.textContent = celcius;
+                    }
+                    else {
+                        tempSpan.textContent = "F";
+                        temp.textContent = temperature;
+                    }
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        })        
+    }
+    else {
+        h1.textContent = "must allow location";
+    }
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace("-", "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
